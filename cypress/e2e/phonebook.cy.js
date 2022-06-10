d80d04c4d71b55004d4993e825c6d44dd98a2c5c
Antti <hellsten.antti@gmail.com>
2022-06-10 09:36:12 +0300

    added cypress e2e testing

---
+++ cypress/e2e/phonebook.cy.js
@@ -0,0 +1,8 @@
+describe("Phonebook app", function() {
+  it("front page can be opened", function() {
+    cy.visit("http://localhost:3001")
+    cy.contains("Phonebook")
+    cy.contains("Add person")
+    cy.contains("Numbers")
+  })
+})
